0e7245ee7cd89b6423ff2f8a98209cc448d750ca
SajjadSeifi <s.09334497255@gmail.com>
2022-02-18 11:14:11 -0800

    impliment seeks and eof

--- lexer.py
+++ lexer.py
@@ -1,3 +1,4 @@
+
 class Lexer:
     def __init__(self,path) -> None:
         self.__init__()
@@ -5,7 +6,29 @@ class Lexer:
         self.currow = 0
         self.curcol = 0
     
+    def nextch(self): 
+        return self.file.read(1)
+
+    def seekprev(self):
+        ptel = self.file.tell() - 1 
+        self.file.seek(ptel,0)
+
+    def seeknext(self):
+        return self.nextch()
+    
+    def eof(self):
+        tok = self.follow()
+        return tok == None
+
     def next():
         pass
     def follow():
         pass
+    
+class LexerGrud(Lexer):
+    def next():
+        pass
+    def follow():
+        pass
+    
+    
