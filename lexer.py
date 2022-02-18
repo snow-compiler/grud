ec8c6c18ae4b00fc16e1ec9fd8d8554225249230
SajjadSeifi <s.09334497255@gmail.com>
2022-02-18 11:02:47 -0800

    impliemnt : lexer abstract calss

---
+++ lexer.py
@@ -0,0 +1,11 @@
+class Lexer:
+    def __init__(self,path) -> None:
+        self.__init__()
+        self.file = open(path,"r")
+        self.currow = 0
+        self.curcol = 0
+    
+    def next():
+        pass
+    def follow():
+        pass
