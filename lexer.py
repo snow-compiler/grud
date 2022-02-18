c42ebf6b47ec28ca48ddf5728dff587de8e96e71
SajjadSeifi <s.09334497255@gmail.com>
2022-02-18 13:07:17 -0800

    impliment grud lexer with lexer

--- lexer.py
+++ lexer.py
@@ -7,28 +7,92 @@ class Lexer:
         self.curcol = 0
     
     def nextch(self): 
-        return self.file.read(1)
+        c = self.file.read(1)
+        
+        if c == '\n':
+            self.curcol = 0 
+            self.currow += 1
+        else:
+            self.curcol += 1
+
+        return c
 
     def seekprev(self):
-        ptel = self.file.tell() - 1 
+        ptel = self.file.tell() - 1
+        if ptel > -1:
+            self.file.seek(ptel,0)
+            c = self.file.read(1)
+            if c == '\n':
+                self.currow -= 1
+            
+
         self.file.seek(ptel,0)
 
     def seeknext(self):
         return self.nextch()
     
     def eof(self):
-        tok = self.follow()
-        return tok == None
+        return self.follow() == None
 
-    def next():
-        pass
-    def follow():
-        pass
+    def endl(self,c):
+        return c == '\n'
+    def none(self,c):
+        return c == ''
+        
+    def skipwspace(self):
+        c = self.nextch()
+        
+        while c.isspace():
+            c = self.nextch()
+
+        self.seekprev()
     
+    def tokseek(self,t:str):
+        if t != '':
+            t = t[:-1]
+            self.seekprev()
+        
+        return t
+
+    def next(self):
+        pass
+    def follow(self):
+        saved = self.file.tell()
+        t = self.next()
+        self.file.seek(saved,0)
+        return t
+
 class LexerGrud(Lexer):
-    def next():
-        pass
-    def follow():
-        pass
+    def next(self):
+        c = self.nextch()
+
+        if c == '' or c == '\n':
+            return c
+
+        self.skipwspace()
+        tok = self.block()
+        
+        return tok
     
-    
+    def skipwspace(self):
+        c = self.nextch()
+        
+        while c.isspace():
+            if self.endl(c):
+                break
+            c = self.nextch()
+
+        self.seekprev()
+
+    def block(self):
+        c = self.nextch()
+        t = ''
+        while c.isspace():
+            if self.endl(c) or self.none(c):
+                break
+            t += c
+            c = self.nextch()
+        
+        t = self.tokseek(t)
+
+        return t
