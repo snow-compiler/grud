6d18f51bb949d6549760292bc3b827bbb523a2f7
SajjadSeifi <s.09334497255@gmail.com>
2022-02-19 06:18:42 -0800

    test  parser and printing

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-from test import test_lexer_linear
 
-test_lexer_linear()
+import json
+from posixpath import abspath
+from gast import ASTKeyPair, ASTVaulePair
+from gtree import GrudTree
+from lexer import LexerGrud
+from gparser import ParserGrud
+
+path = abspath("./test/tslang.grud")
+lex = LexerGrud(path)
+parser = ParserGrud(lex)
+gtree = parser.parse()
+
+for key in gtree:
+    g3 : GrudTree = gtree[key]
+    print("[:: %s ::]" % key)
+    for k in g3.tbl:
+        if isinstance(k,ASTKeyPair):
+            print(k.key)
+        if isinstance(k,ASTVaulePair):
+            print(k.value)
