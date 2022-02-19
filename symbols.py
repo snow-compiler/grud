36c51b7080e784ead49aa9379de9938177fd8df6
SajjadSeifi <s.09334497255@gmail.com>
2022-02-19 00:57:05 -0800

    build grud symbols module base

--- symbols.py
+++ symbols.py
@@ -14,3 +14,19 @@ class Symbols:
     def find(self):
         pass
     
+class GrudSymbols ( Symbols ):
+    def __init__(self) -> None:
+        super().__init__()
+        self.list = []
+
+    def put(self,key : str, value : str):
+        pass
+    
+    def rm(self,key : str):
+        pass
+    
+    def get(self,key : str):
+        pass
+    
+    def find(self,key : str):
+        pass
