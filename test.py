fea5183e275740d8617fafd7f40962b8a3e475ad
SajjadSeifi <s.09334497255@gmail.com>
2022-02-19 12:03:24 -0800

    add test case for parser tree

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from posixpath import abspath
-from lexer import Lexer, LexerGrud
+from lexer import LexerGrud
+from gtree import GrudTree
+from lexer import LexerGrud
+from gparser import ParserGrud
+
 
 def test_lexer():
     path = abspath("./test/tslang.grud")
@@ -24,3 +28,12 @@ def test_lexer_linear():
 
         print(tokl)
  
+def test_parser_tree():
+    path = abspath("./test/tslang.grud")
+    lex = LexerGrud(path)
+    parser = ParserGrud(lex)
+    gtree = parser.parse()
+
+    for key in gtree:
+        g3 : GrudTree = gtree[key]
+        GrudTree.print(g3.tbl,key)
