cb11f93acbece0c057e42b41fa0e7f4ba131cd26
SajjadSeifi <s.09334497255@gmail.com>
2022-02-19 00:50:58 -0800

    impliment asbstract class  Symbols

---
+++ symbols.py
@@ -0,0 +1,16 @@
+class Symbols:
+    def __init__(self) -> None:
+        self.parent = None
+    
+    def put(self):
+        pass
+    
+    def rm(self):
+        pass
+    
+    def get(self):
+        pass
+    
+    def find(self):
+        pass
+    
