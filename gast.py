604f8dd5864896343c0ec460efbf2a5ed1f834ec
SajjadSeifi <s.09334497255@gmail.com>
2022-02-20 01:57:37 -0800

    impliments index & build index for value ast

--- gast.py
+++ gast.py
@@ -6,7 +6,7 @@ class AST:
         self.parent = None
         self.id = id
         self.children = []
-        self.value = {}
+        self.val = {}
     
     def first(self):
         return self.children[0]
@@ -18,10 +18,10 @@ class AST:
         self.children.append(child)
 
     def setval(self,key:str,val:Any):
-        self.value[key] = val
+        self.val[key] = val
     
     def getval(self,key:str):
-        return self.value.get(key)
+        return self.val.get(key)
 
 class ASTKeyPair( AST ):
     def __init__(self,key:str) -> None:
@@ -32,3 +32,15 @@ class ASTVaulePair ( AST ):
     def __init__(self,value) -> None:
         super().__init__(uuid.uuid4())
         self.value = value
+
+    def setidx(self,index):
+        self.setval("index",index)
+
+    def getidx(self):
+        return self.getval("index")
+
+    def setbuildidx(self,buildidx):
+        self.setval("build:index",buildidx)
+
+    def getbuildidx(self):
+        return self.getval("build:index")
